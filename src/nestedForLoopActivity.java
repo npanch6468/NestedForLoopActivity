7f5d9e86d58ad42e87bd2c5701441526d2c2033b
npanch6468 <nagapanchumarty@gmail.com>
2019-06-12 16:33:28 -0400

    first commit

---
+++ src/nestedForLoopActivity.java
@@ -0,0 +1,11 @@
+public class nestedForLoopActivity {
+    public static void main(String [] args){
+        int row = 9;
+        for(int i = 1; i <= row; ++i){
+            for(int j = 1; j <= i; ++j){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
